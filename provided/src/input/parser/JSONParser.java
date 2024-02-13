71e4aca9352909a1c2916e48efc6db1fbcf27614
JacksonT16 <jacksontedesco@yahoo.com>
2024-02-13 14:15:53 -0500

    parser

--- provided/src/input/parser/JSONParser.java
+++ provided/src/input/parser/JSONParser.java
@@ -34,21 +34,60 @@ public class JSONParser
 		// Parsing is accomplished via the JSONTokenizer class.
 		JSONTokener tokenizer = new JSONTokener(str);
 		JSONObject  JSONroot = (JSONObject)tokenizer.nextValue();
-		
-		String description = "";
+
+		String description = JSONroot.getString("Description");
 		PointNodeDatabase points = new PointNodeDatabase();
 		SegmentNodeDatabase segments = new SegmentNodeDatabase();
 		
-		while(tokenizer.more()) {
-			String next = tokenizer.nextString();
+		//ArrayList<PointNode> pointlist = JSONroot.getJSONArray("Points")
+		
+		for(PointNode point:JSONroot.getJSONArray("Points")) {
+			String name = JSONroot.getString("name");
+			double x = JSONroot.getInt("x");
+			double y = JSONroot.getInt("y");
 			
-			if(next == "Description") {
+			points.put(new PointNode(name, x, y));
+		}
+		
+		for(Object Segment:JSONroot.getJSONArray("Segments")) {
+			segments.addUndirectedEdge(new SegmentNode());
+		}
+		
+		
+		
+		
+		/*while(tokenizer.more()) {
+			String next = tokenizer.nextString('"');
+
+			if(next.equals("Description")) {
+				description = tokenizer.nextString('"');
+			}
+
+			if(next.equals("Points")) {
+				String name = "";
+				double x = 0;
+				double y = 0;
+
+				if(tokenizer.nextString('"').equals("name")) {
+					name = tokenizer.nextString('"');
+				}
+				if(tokenizer.nextString('"').equals("x")){
+					x = (double) tokenizer.nextValue();//test to make sure this does not get the ":"
+				}
+				if(tokenizer.nextString('"').equals("y")){
+					y = (double) tokenizer.nextValue();// test to make sure this does not get the ":"
+				}
+				
+				points.put(new PointNode(name, x, y));
+			}
+			
+			if(next.equals("Segments")) {
 				
 			}
-		}
-        // TODO: Build the whole AST, check for return class object, and return the root
+		}*/
+		// TODO: Build the whole AST, check for return class object, and return the root
 	}
 
-    // TODO: implement supporting functionality
+	// TODO: implement supporting functionality
 
 }
