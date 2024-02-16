7c13f1950ea1e0b0cecb27d29a07e46497d1ab41
CaseRiddle056 <132170214+CaseRiddle056@users.noreply.github.com>
2024-02-16 15:03:09 -0500

    FigureNodeTest

--- provided/test/input/Tree/FigureNodeTest.java
+++ provided/test/input/Tree/FigureNodeTest.java
@@ -47,9 +47,32 @@ class FigureNodeTest {
 		System.out.(node.unparse(st, 0));*/
 	}
 
+	@Test
+    void unparseTest() {
+        PointNodeDatabase pointsDatabase = new PointNodeDatabase();
+        pointsDatabase.put(new PointNode("A", 1.0, 2.0));
+        pointsDatabase.put(new PointNode("B", 3.0, 4.0));
 
-	FigureNode figure = new FigureNode("Sample Figure", pointsDatabase, segmentDatabase);
+        SegmentNodeDatabase segmentDatabase = new SegmentNodeDatabase();
+        segmentDatabase.addUndirectedEdge(new PointNode("A", 1.0, 2.0), new PointNode("B", 3.0, 4.0));
 
-	StringBuilder result = new StringBuilder();
+        FigureNode figure = new FigureNode("Sample Figure", pointsDatabase, segmentDatabase);
+
+        StringBuilder result = new StringBuilder();
+
+        figure.unparse(result, 0);
+        
+        String expectedOutput = "Description : \"Sample Figure\"\n" +
+                "Points:\n" +
+                "    Point(A)(1.0, 2.0)\n" +
+                "    Point(B)(3.0, 4.0)\n" +
+                "Segments:\n" +
+                "    Segment(A, B)\n";
+        assertEquals(expectedOutput, result.toString());
+    }
+
+	//FigureNode figure = new FigureNode("Sample Figure", pointsDatabase, segmentDatabase);
+
+	//StringBuilder result = new StringBuilder();
 }
 
