77321b274d6df44e69cca44aa3f7fa37c980af99
CaseRiddle056 <132170214+CaseRiddle056@users.noreply.github.com>
2024-02-20 14:48:42 -0500

    Update PointNodeTest

--- provided/test/input/Tree/PointNodeTest.java
+++ provided/test/input/Tree/PointNodeTest.java
@@ -22,42 +22,58 @@ class PointNodeTest {
 	void testEquals() {
 		PointNode myPointNode= new PointNode(1,2);
 		PointNode mySecondPointNode= new PointNode(1,2);
-		
+
 		assertTrue(myPointNode.equals(mySecondPointNode));
 		mySecondPointNode= new PointNode(3,2);
 		assertFalse(myPointNode.equals(mySecondPointNode));
-		
+
 		myPointNode = new PointNode(0.99999999999,0.999999999999);
 		mySecondPointNode = new PointNode(1,1);
 		assertTrue(myPointNode.equals(mySecondPointNode));
 	}
-	
+
 	@Test
 	void testToString() {
 		PointNode myPointNode= new PointNode(1,2);
 		assertEquals(("__UNNAMED(1.0, 2.0)"), myPointNode.toString());
-		
+
 		myPointNode= new PointNode("joe", 1,2);
 		assertEquals(("joe(1.0, 2.0)"), myPointNode.toString());
 	}
-	
+
 	@Test
 	void testUnparse() {
-		List<PointNode> points = new ArrayList<>();
-		points.add(new PointNode("Point1", 1.0, 2.0));
-		points.add(new PointNode("Point2", 3.0, 4.0));
-		points.add(new PointNode("Point3", 5.0, 6.0));
-		PointNodeDatabase database = new PointNodeDatabase(points);
+		List<PointNode> p1 = new ArrayList<>();
+		p1.add(new PointNode("Point1", 1.0, 2.0));
+		p1.add(new PointNode("Point2", 3.0, 4.0));
+		p1.add(new PointNode("Point3", 5.0, 6.0));
+		PointNodeDatabase d1 = new PointNodeDatabase(p1);
 
-		StringBuilder sb = new StringBuilder();
+		StringBuilder s1 = new StringBuilder();
 
-		database.unparse(sb, 0);
+		d1.unparse(s1, 0);
 
-		String expectedOutput = "Point1: (1.0, 2.0)" +
+		String e1 = "Point1: (1.0, 2.0)" +
 				"Point2: (3.0, 4.0)" +
 				"Point3: (5.0, 6.0)";
 
-		assertEquals(expectedOutput, sb.toString());
+		assertEquals(e1, s1.toString());
+
+		List<PointNode> p2 = new ArrayList<>();
+		p2.add(new PointNode("Point1", 0.0, 0.0));
+		p2.add(new PointNode("Point2", 3.0, 4.0));
+		p2.add(new PointNode("Point3", 5.0, 6.0));
+		PointNodeDatabase d2 = new PointNodeDatabase(p2);
+
+		StringBuilder s2 = new StringBuilder();
+
+		d2.unparse(s2, 0);
+
+		String e2 = "Point1: (0.0, 1.0)" +
+				"Point3: [3.0, 4.0]" +
+				"Point2: (5.0, 6.0)";
+
+		assertNotEquals(e2, s2.toString());
 	}
 
 }
